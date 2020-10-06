4ae923ba9b590fc1d6dfdece03e1eeed3621c2ca
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-10-05 21:45:02 -0500

    update js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 function successful_submission() {
-    window.location.replace("https://itm-362.github.io/Project-1/success")
+    window.location.replace("https://itm-362.github.io/Project-1/success.html")
 }
