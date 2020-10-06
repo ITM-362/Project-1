af540eb474173690f1740139e04ca8eb87f1435f
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-10-05 22:53:30 -0500

    fix js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ document.getElementById("submit_button").onclick = function() { confirm() };
 function confirm() {
     document.getElementById("submit_button").innerHTML = "We will notify you when you iPhone ships";
 }
+window.addEventListener('load', ini);
