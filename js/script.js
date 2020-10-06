7edd1df2e10c591626f219c75e14d4ae6899435b
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-10-05 23:31:12 -0500

    changed document to button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function confirm() {
     var button = document.getElementById("submit_button");
-    document.addEventListener("click", function() {
+    button.addEventListener("click", function() {
 
         alert("We will notify you when your iPhone ships");
     });
