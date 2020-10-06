a3736d72d743c4dff15c4c0f365fa0fb73f471a3
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-10-05 23:28:02 -0500

    removed slash from js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function confirm() {
     var button = document.getElementById("submit_button");
     document.addEventListener("click", function() {
 
-        alert("We will notify you when you iPhone ships");
+        alert("We will notify you when your iPhone ships");
     });
 }
 window.addEventListener('load', confirm);
