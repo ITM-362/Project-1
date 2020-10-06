3cbd1f1d15640fbfb344d98e390f55244fdf2ec3
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-10-05 22:37:05 -0500

    fixed js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.getElementById("confirm").onclick = function() { confirm() };
+document.getElementById("submit_button").onclick = function() { confirm() };
 
 function confirm() {
     document.getElementById("confirm").innerHTML = "We will notify you when you iPhone ships";
