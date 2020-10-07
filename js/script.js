22dbaa7b8c2452da2e666babe68df8e48e56a788
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-10-06 23:18:03 -0500

    fixed javascript

--- js/script.js
+++ js/script.js
@@ -1,33 +1,11 @@
-$(document).ready(function() {
-    $("#send").click(function(e) {
-        e.preventDefault();
-        var email = $("#email").val();
-        var phone_number = $("#phone_number").val();
-        var address = $("#address").val();
-        var age = $("#age").val();
-
-        if (!(email == '' || phone_number == '' || address == '' || age == '')) {
-            $("#submitdata").empty();
-            $("#submitdata").append("Email: " + email + "<br/>Phone number: " + phone_number + "<br/>Age: " + age);
-            alert("We will notify you when your iPhone ships");
-        } else {
-            alert("Please Fill All Fields.");
-        }
-    });
-});
-Copy
-
-
-// function confirm() {
-
-// var button = document.getElementById("submit_button");
-// button.addEventListener("click", function() {
-// if (document.getElementsByClassName("input") != null) {
-// alert("We will notify you when your iPhone ships");
-// } else {
-// alert("Please fill out the form");
-// }
-
-// });
-// }
-// window.addEventListener('load', confirm);
+function buttonPress() {
+    var email = document.getElementById("email_input").value;
+    var phone_number = document.getElementById("phone_input").value;
+    var address = document.getElementById("address_input").value;
+    var age = document.getElementById("number_input").value;
+    if (email && phone_number && address && age) {
+        alert("We will notify you when your iPhone ships");
+    } else {
+        alert("Please fill out the form");
+    }
+}
