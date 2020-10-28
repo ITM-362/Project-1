2ab7de266b4844b84f7bf03ef86427457246404b
EricLiIIT <60123235+EricLiIIT@users.noreply.github.com>
2020-10-28 18:04:34 -0500

    fixed JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-function buttonPress() {
-    var email = document.getElementById("email_input").value;
-    var phone_number = document.getElementById("phone_input").value;
-    var address = document.getElementById("address_input").value;
-    var age = document.getElementById("number_input").value;
-    if (email && phone_number && address && age) {
-        alert("We will notify you when your iPhone ships");
-    } else {
-        alert("Please fill out the form");
-    }
+function init() {
+    var button = document.getElementById("submit_button");
+    button.addEventListener("click", function buttonPress() {
+        var email = document.getElementById("email_input").value;
+        var phone_number = document.getElementById("phone_input").value;
+        var address = document.getElementById("address_input").value;
+        var age = document.getElementById("number_input").value;
+        if (email && phone_number && address && age) {
+            alert("We will notify you when your iPhone ships");
+        } else {
+            alert("Please fill out the form");
+        }
+    })
+    window.addEventListener('load', init);
 }
